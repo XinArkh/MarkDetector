ff608ed5efc7bb930ce4d1ce97abb6cec76fcbaf
Wu Hsin <wuxin.zju@qq.com>
2018-05-22 13:04:30 +0800

    将数据点-->角度的处理程序整合到detector类里面；下一步还需要优化角度算法，识别正负

--- detector.py
+++ detector.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import os
+import re
 
 
 class detector:
@@ -188,9 +189,49 @@ class detector:
         self._close()
 
 
+    def data2angle(self):
+        f = open('data/' + self.name + str(self.number) + '.txt', 
+                'r', encoding='utf-8')
+        if not os.path.isdir('angle'):
+            os.makedirs('angle')
+        output = open('angle/' + self.name + str(self.number) + '.txt', 
+        'a', encoding='utf-8')
+        lines = f.readlines()
+        for line in lines:
+            print(line)
+            if line == '***Start***\n':
+                output.write(line)
+                continue
+            items = re.sub('\[|\(| |\)|\]|\n', '', line)
+            items = re.split(',' ,items)
+            # x1, y1, x2, y2, x3, y3 = [1, 0, 0, 1, 1, 1]
+            x1, y1, x2, y2, x3, y3 = [int(a) for a in items]
+            # 以下为（x，y）坐标系
+            vector0 = np.array([0,1])
+            s0 = np.sqrt(vector0.dot(vector0))
+
+            vector1 = np.array([x2-x1, y2-y1])
+            s1 = np.sqrt(vector1.dot(vector1))
+
+            vector2 = np.array([x3-x2, y3-y2])
+            s2 = np.sqrt(vector2.dot(vector2))
+
+            cos_t = vector0.dot(vector1)/(s0*s1)
+            cos_a = vector1.dot(vector2)/(s1*s2)
+            # print(cos_t, cos_a)
+            angle_t = np.arccos(cos_t)*180/np.pi
+            angle_a = np.arccos(cos_a)*180/np.pi
+            # print(angle_t, angle_a)
+            output.write(str(angle_t)+' '+str(angle_a)+'\n')
+
+        f.close()
+        output.close()
+
+
 if __name__ == '__main__':
     d = detector(1, 'walk', 1)
     d.getTemplate('template.jpg')
     d.saveVideo()
     d.saveData()
     d.run(0.9)
+    d.data2angle()
